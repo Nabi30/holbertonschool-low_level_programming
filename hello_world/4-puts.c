773a42b8c4dca0a9d83779cf078f71c0dd2bf930
Nabi30 <nabiguzaryan@gmail.com>
2024-02-20 23:22:23 +0100

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -10,10 +10,7 @@
  *        **/
 int main(void)
 {
-<<<<<<< HEAD
-=======
-		/* Print the desired string using the puts function */
->>>>>>> c481b51829c2b591e72a159398f515a96c2ab7c9
+
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
