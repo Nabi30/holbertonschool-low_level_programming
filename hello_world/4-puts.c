c481b51829c2b591e72a159398f515a96c2ab7c9
Nabi30 <159523366+Nabi30@users.noreply.github.com>
2024-02-20 14:53:48 +0100

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 		/* Print the desired string using the puts function */
-		puts("Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
 
