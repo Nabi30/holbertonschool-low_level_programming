878df1bdb94cfdd2348ec0adafc5c11ec1517ec8
Nabi30 <nabiguzaryan@gmail.com>
2024-02-20 23:26:27 +0100

    with proper grammar, but the outcome is a piece of art,

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -12,20 +11,3 @@ int main(void)
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
-=======
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: This program prints "with proper grammar,
- * but the outcome is a piece of art," using the puts function.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
->>>>>>> 715f322ef6e16e16760360ecc250a96762f75bee
