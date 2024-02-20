14ea424054bc60b77e5a8805ec4bf6b5bcbb7bdb
Nabi30 <159523366+Nabi30@users.noreply.github.com>
2024-02-20 15:24:23 +0100

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: This program prints "Programming is like building
- *     * a multilingual puzzle" using the puts function.
- *      *
- *       * Return: Always 0 (Success)
- *        **/
+  * main - Entry point
+  * Description: This program prints "Programming is like building
+  * a multilingual puzzle" using the puts function.
+  * Return: Always 0 (Success)
+  **/
 int main(void)
 {
-<<<<<<< HEAD
-=======
-		/* Print the desired string using the puts function */
->>>>>>> c481b51829c2b591e72a159398f515a96c2ab7c9
+
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
