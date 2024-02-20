f7c726504655f98c0e3674faf15d45b92123b6e1
Nabi30 <nabiguzaryan@gmail.com>
2024-02-20 15:16:45 +0100

    Task four

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: This program prints "Programming is like building
+ *     * a multilingual puzzle" using the puts function.
+ *      *
+ *       * Return: Always 0 (Success)
+ *        **/
 int main(void)
 {
-		/* Print the desired string using the puts function */
-		puts("Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
