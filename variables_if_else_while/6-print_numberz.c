a4752c6e8ac39aab433370e8c943233a1adb1f20
Nabi30 <nabiguzaryan@gmail.com>
2024-02-23 12:43:22 +0100

    print numbers

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -4,17 +4,9 @@ int main(void)
 {
     /* Print single digit numbers from 0 to 9 */
 putchar('0');
-putchar('1');
-putchar('2');
-putchar('3');
-putchar('4');
-putchar('5');
-putchar('6');
-putchar('7');
-putchar('8');
-putchar('9');
+putchar('1' + 1); /* Equivalent to '2'*/
 putchar('\n');
-/* New line */
+    /* New line */
 return (0);
 }
 
