04110cf15aba46104d588deba29f86011a34be2d
Nabi30 <nabiguzaryan@gmail.com>
2024-02-23 22:43:37 +0100

    projects C

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - main function
+*Return: alaways (0)
+*/
+int main(void)
+{
+char s[] = "_putchar\n"; /*create a string with _putchar and a new line*/
+write(1, s, 8); /*write the string to the standard output*/
+return (0); /*return 0*/
+}
+
