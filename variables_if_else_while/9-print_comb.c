8239c67f60cf01010887863af6ae59b4cb5f352f
Nabi30 <159523366+Nabi30@users.noreply.github.com>
2024-02-23 17:40:48 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: alaways (0)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; ++i)
+{putchar(i + '0'); /*Print the digit as a character*/
+if (i < 9)
+{
+putchar(','); /*Separate with a comma*/
+putchar(' '); /*Followed by a space*/
+}
+}
+putchar('\n'); /* Print a newline character*/
+return (0);
+}
