887972db85668745eb35714deb9fe5744a2cfb93
Nabi30 <nabiguzaryan@gmail.com>
2024-02-27 15:09:56 +0100

    0. _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - main function
-*Return: alaways (0)
+* main - Entry point
+* Return: alaways (0)
 */
 int main(void)
 {
-char s[] = "_putchar\n"; /*create a string with _putchar and a new line*/
-write(1, s, 8); /*write the string to the standard output*/
+/*create a string with _putchar and a new lin*/
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+/*write the string to the standard output*/
+_putchar('\n');
 return (0); /*return 0*/
 }
-
