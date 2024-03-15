f3b25dd97daf423a55992cdb19dbdc97f293a9a5
Nabi30 <nabiguzaryan@gmail.com>
2024-03-15 13:57:50 +0100

    Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,10 +2,12 @@
 #define DOG_H
 
 /**
-* struct dog - structure definition of a dog
-* @name: character string
-* @age: integer
-* @owner: character string
+* struct dog - struct for dog info
+* @name: type char
+* @age: float type
+* @owner: char type
+*
+*Description: structure to store dog info
 */
 typedef struct dog
 {
@@ -15,7 +17,16 @@ char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
 #endif
+
