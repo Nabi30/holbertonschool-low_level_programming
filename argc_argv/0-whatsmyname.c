38ab8786f0a5c20d6a8311dca91414092d8e3b24
Nabi30 <nabiguzaryan@gmail.com>
2024-03-15 11:00:02 +0100

    0. It ain't what they call you, it's what you answer to

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,17 +2,13 @@
 #include <stdio.h>
 
 /**
-* main - print the name of the file followed by a new line
-* @argc: argument count
-* @argv: arguments passed to main as an array of strings
-* Return: 0
-*/
-
-int main(int argc, char *argv[])
+ * main - program to print name of program
+ * @argc: size of argv array and number of command line arguments
+ * @argv: An array of size argc
+ * Return: always successful
+ */
+int main(int argc __attribute__((unused)), char **argv)
 {
-(void) argc;
-
 printf("%s\n", argv[0]);
-
 return (0);
 }
