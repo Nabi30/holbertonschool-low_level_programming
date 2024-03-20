9dc6dd137aca2a57e40e213d77b33431e9987d19
Nabi30 <nabiguzaryan@gmail.com>
2024-03-20 11:01:06 +0100

    headers

--- function_pointers/_putchar.c
+++ function_pointers/_putchar.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**_putchar - writes the character c ti stdiut
- *  * @c : the character to print
- *   * Return : On success 1.
- *    * On error, -1 is returned, and error is set approptiately.
- */
+* @c : the character to print
+* Return : On success 1.
+* On error, -1 is returned, and error is set approptiately.
+*/
 
 int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-
