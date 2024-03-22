39ca5c363ba3a39828f6080597065ac29260258c
Nabi30 <nabiguzaryan@gmail.com>
2024-03-22 10:23:19 +0100

    0. Beauty is variable, ugliness is constant

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,27 +2,25 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - variadic function to add number
- * @n: const unsigned int type
- * Return: sum of numbers
- */
+* sum_them_all - variadic function to add number
+* @n: const unsigned int type
+* Return: sum of numbers
+*/
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
-	unsigned int i;
-	va_list addlist;
+unsigned int sum = 0;
+unsigned int i;
+va_list addlist;
 
-	va_start(addlist, n);
+va_start(addlist, n);
 
-	if (n == 0)
-		return (0);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(addlist, unsigned int);
-	}
-	va_end(addlist);
-
-	return (sum);
+if (n == 0)
+	return (0);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(addlist, unsigned int);
 }
-
+va_end(addlist);
+return (sum);
+}
