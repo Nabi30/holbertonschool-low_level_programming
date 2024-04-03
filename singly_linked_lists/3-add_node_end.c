90582964b130d0a7095f62582ca74936229d93e9
Nabi30 <nabiguzaryan@gmail.com>
2024-04-03 17:18:11 +0200

    3. Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 /**
- * add_node_end - Adds a new node at the end of a list_t list.
- * @head: A double pointer to the head of the list_t list.
- * @str: The string to be duplicated and added to the list.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
+* add_node_end - Adds a new node at the end of a list_t list.
+* @head: A double pointer to the head of the list_t list.
+* @str: The string to be duplicated and added to the list.
+*
+* Return: The address of the new element, or NULL if it failed.
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *n_node, *temp;
